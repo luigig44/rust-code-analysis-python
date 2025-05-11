17d4ff7c84c3f8679ea52e146aa367a767bbecae
Luigi Finetti <luigi@finetti.com.ar>
2025-05-11 08:43:16 -0300

    Refactor

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -1,4 +1,4 @@
-// pub mod comment;
+pub mod comment;
 // pub mod function;
 pub mod metrics;
 // pub mod core;
--- src/lib.rs
+++ src/lib.rs
@@ -1,48 +1,25 @@
-use std::path::PathBuf;
-
 use pyo3::prelude::*;
-
-use rust_code_analysis::{AstCallback, AstCfg, AstPayload, LANG, action, guess_language};
-use rust_code_analysis::{Callback, ParserTrait, rm_comments};
 
 mod backend;
 use backend::metrics::{MetricsPayload, metrics_rust};
-/// Unit structure to implement the `Callback` trait.
-#[derive(Debug)]
-pub struct CommentRemoval;
+use backend::comment::{CommentRemovalPayload, comment_removal_rust};
 
-impl Callback for CommentRemoval {
-    type Res = Option<String>;
-    type Cfg = ();
-
-    fn call<T: ParserTrait>(_cfg: Self::Cfg, parser: &T) -> Self::Res {
-        rm_comments(parser).map(|v| String::from_utf8_lossy(&v).into_owned())
-    }
-}
 
 #[pyfunction]
 fn comment_removal(file_name: String, code: String) -> PyResult<String> {
-    let path = PathBuf::from(file_name);
-    let buf = code.into_bytes();
-    let (language, _) = guess_language(&buf, path);
-    if let Some(language) = language {
-        let language = if language == LANG::Cpp {
-            LANG::Ccomment
-        } else {
-            language
-        };
-        action::<CommentRemoval>(&language, buf, &PathBuf::from(""), None, ()).ok_or_else(|| {
-            PyErr::new::<pyo3::exceptions::PyValueError, _>("Failed to remove comments")
-        })
-    } else {
-        Err(PyErr::new::<pyo3::exceptions::PyValueError, _>(
-            "Invalid language",
-        ))
-    }
+    let payload = CommentRemovalPayload {
+        file_name,  
+        code,
+    };
+    let response = comment_removal_rust(payload);
+
+    response
+        .map(|bytes| String::from_utf8_lossy(&bytes).into_owned())
+        .map_err(|e| PyErr::new::<pyo3::exceptions::PyValueError, _>(e))
 }
 
 #[pyfunction]
-fn metrics_py(file_name: String, code: String, unit: bool) -> PyResult<Py<PyAny>> {
+fn metrics(file_name: String, code: String, unit: bool) -> PyResult<Py<PyAny>> {
     let payload = MetricsPayload {
         file_name,
         code,
@@ -65,6 +42,6 @@ fn metrics_py(file_name: String, code: String, unit: bool) -> PyResult<Py<PyAny>
 #[pymodule]
 fn rust_code_analysis_python(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(comment_removal, m)?)?;
-    m.add_function(wrap_pyfunction!(metrics_py, m)?)?;
+    m.add_function(wrap_pyfunction!(metrics, m)?)?;
     Ok(())
 }
